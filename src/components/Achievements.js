48491daf5e6f7d40d173fd20d0b0f3fc5e0852fc
aaronk2002 <aaronakj2002@gmail.com>
2022-11-01 16:32:34 -0400

    Modularized Achievement Page

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -14,61 +14,52 @@ function Desc(props) {
 function Img(props) {
     return (<>
         <a href={props.link} target="_blank">
-            <img src={require(props.image)} className="aaron" />
+            <img src={props.img} className="portrait" />
         </a>
     </>)
 }
 
 function Achievements() {
-    const comp_desc = [
+    const comps = [
         {
             title: "Honorable Mention, Putnam Mathematical Competition 2021",
-            detail: "Received 50/120 points, ranked 57 out of 2975 people"
+            detail: "Received 50/120 points, ranked 57 out of 2975 people",
+            link: "https://www.maa.org/sites/default/files/pdf/Putnam/2021/AnnouncementOfWinnersFall2021.docx%20%284%29.pdf",
+            img: require("../public/Aaron.PNG")
         },
         {
             title: "Silver Medal, International Mathematical Olympiad 2021",
-            detail: "Received 21/42 points, ranked 105 out of 619 people"
+            detail: "Received 21/42 points, ranked 105 out of 619 people",
+            link: "https://www.imo-official.org/team_r.aspx?code=IDN&year=2021&column=total&order=desc&gender=hide",
+            img: require("../public/Aaron.PNG")
         },
         {
             title: "Gold Medal, International Mathematical Olympiad 2020",
-            detail: "Received 33/42 points, ranked 22 out of 616 people"
+            detail: "Received 33/42 points, ranked 22 out of 616 people",
+            link: "https://www.imo-official.org/team_r.aspx?code=IDN&year=2020&column=total&order=desc&gender=hide",
+            img: require("../public/Aaron.PNG")
         },
         {
             title: "Silver Medal, International Mathematical Olympiad 2019",
-            detail: "Received 27/42 points, ranked 101 out of 621 people"
+            detail: "Received 27/42 points, ranked 101 out of 621 people",
+            link: "https://www.imo-official.org/team_r.aspx?code=IDN&year=2019",
+            img: require("../public/Aaron.PNG")
         },
         {
             title: "Bronze Medal, Romanian Master of Mathematics 2019",
-            detail: "Received 25/42 points, ranked 64 out of 135 people"
+            detail: "Received 25/42 points, ranked 64 out of 135 people",
+            link: "https://rmms.lbi.ro/rmm2019/index.php?id=results_math",
+            img: require("../public/Aaron.PNG")
         }
     ];
-    return (<>
-        <SingleContent 
-            desc={<Desc title={comp_desc[0].title} detail={comp_desc[0].detail} />}
-            img={<a href="https://www.maa.org/sites/default/files/pdf/Putnam/2021/AnnouncementOfWinnersFall2021.docx%20%284%29.pdf" target="_blank">
-                <img src={require("../public/Aaron.PNG")} className="portrait" />
-            </a>} />
-        <SingleContent 
-            desc={<Desc title={comp_desc[1].title} detail={comp_desc[1].detail} />}
-            img={<a href="https://www.imo-official.org/team_r.aspx?code=IDN&year=2021&column=total&order=desc&gender=hide" target="_blank">
-                <img src={require("../public/Aaron.PNG")} className="portrait" />
-            </a>} />
-        <SingleContent 
-            desc={<Desc title={comp_desc[2].title} detail={comp_desc[2].detail} />}
-            img={<a href="https://www.imo-official.org/team_r.aspx?code=IDN&year=2020&column=total&order=desc&gender=hide" target="_blank">
-                <img src={require("../public/Aaron.PNG")} className="portrait" />
-            </a>} />
-        <SingleContent 
-            desc={<Desc title={comp_desc[3].title} detail={comp_desc[3].detail} />}
-            img={<a href="https://www.imo-official.org/team_r.aspx?code=IDN&year=2019" target="_blank">
-                <img src={require("../public/Aaron.PNG")} className="portrait" />
-            </a>} />
-        <SingleContent 
-            desc={<Desc title={comp_desc[4].title} detail={comp_desc[4].detail} />}
-            img={<a href="https://rmms.lbi.ro/rmm2019/index.php?id=results_math" target="_blank">
-                <img src={require("../public/Aaron.PNG")} className="portrait" />
-            </a>} />
-    </>);
+    return (
+        comps.map((comp) => <>
+            <SingleContent
+                desc={<Desc title={comp.title} detail={comp.detail} />}
+                img={<Img link={comp.link} img={comp.img} />}
+            />
+        </>)
+    );
 }
 
 export default Achievements;
