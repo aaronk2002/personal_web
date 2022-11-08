7479c9972306745a3d977fdca06b83934d027bae
aaronk2002 <aaronakj2002@gmail.com>
2022-11-08 17:11:31 -0500

    Fixed Navigation bar for mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,38 @@
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import "../style/Containers.css";
+import useScreenOrientation from 'react-hook-screen-orientation';
+import "../style/Images.css";
 
 function NavBar() {
+    const [visible, setVisible] = useState(false);
+    const [height, setHeight] = useState("small");
+    const toggle = () => {
+        setVisible(!visible);
+    };
+    const [isPortrait, setIsPortrait] = useState(useScreenOrientation() == "portrait-primary");
+    const changePortrait = () => {setIsPortrait(!isPortrait);};
+    window.addEventListener('orientationchange', changePortrait);
     return (<>
-        <span className="navFlex">
-            <span className="navPages"><Link to="/" className="navLinks"><button className="navButtons">Home</button></Link></span>
-            <span className="navPages"><Link to="/achievements" className="navLinks"><button className="navButtons">Achievements</button></Link></span>
-            <span className="navPages"><Link to="/experiences" className="navLinks"><button className="navButtons">Experiences</button></Link></span>
-            <span className="navPages"><Link to="/projects" className="navLinks"><button className="navButtons">Projects</button></Link></span>
-        </span>
+        <div className={visible && isPortrait ? "big" : "small"}>
+            <span className="navFlex">
+                { isPortrait ?
+                    <><span className="navPages"><div className="navLinks" onClick={toggle}><button className="navButtons"><div className="menu"></div>
+                    <div className="menu"></div>
+                    <div className="menu"></div></button></div></span></>
+                : <></>}
+                {
+                    visible || !isPortrait ?
+                    <>
+                        <span className="navPages"><Link to="/" className="navLinks" onClick={toggle}><button className="navButtons">Home</button></Link></span>
+                        <span className="navPages"><Link to="/achievements" className="navLinks" onClick={toggle}><button className="navButtons">Achievements</button></Link></span>
+                        <span className="navPages"><Link to="/experiences" className="navLinks" onClick={toggle}><button className="navButtons">Experiences</button></Link></span>
+                        <span className="navPages"><Link to="/projects" className="navLinks" onClick={toggle}><button className="navButtons">Projects</button></Link></span>
+                    </>:
+                    <></>
+                }
+            </span>
+        </div>
     </>);
 }
 
